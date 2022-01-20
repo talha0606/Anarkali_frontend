7183fb389a6f3b59d1f03ce658a75e9beb115853
aliuppal9797 <80312742+aliuppal9797@users.noreply.github.com>
2022-01-21 02:14:32 +0500

    css of home page card updated

--- src/components/Allshops.js
+++ src/components/Allshops.js
@@ -41,7 +41,7 @@ const Allshops = ({ shopData }) => {
                   <img
                     // src={`/uploads/${shop.shopImage}`}
                     src={shop.imageUrl}
-                    className="card-img-top card-img img-thumbnail"
+                    className="home-card-img-top card-img img-thumbnail"
                     alt="Card image cap"
                     //   onClick={() => {
                     //     setID(user._id);
